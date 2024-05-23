1802b0146793204c401e3a23de989afd47048d9a
nghiahu <147359597+nghiahu@users.noreply.github.com>
2024-05-24 03:54:12 +0700

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
-import React, { useContext } from 'react'
-import SonContext from './components/Bt1/SonContext'
-import { context } from './components/Bt1/ThemeContext'
-import Button from './components/B3/Button'
+import React from 'react'
+import Bt1 from './components/Bt1'
+import BT2 from './components/BT2'
+import Bt3 from './components/Bt3'
+import Bt4 from './components/Bt4'
+import Bt5 from './components/Bt5'
+import Bt6 from './components/Bt6'
+
 export default function App() {
-  
-  let {them} = useContext(context)
   return (
     <div>
-              <context.Provider value={them}>
-            <SonContext></SonContext>
-            <Button></Button> 
-            </context.Provider>  
-            
+      <Bt1></Bt1>
+      <BT2></BT2>
+      <Bt3></Bt3>
+      <Bt4></Bt4>
+      <Bt5></Bt5>
+      <Bt6></Bt6>
     </div>
   )
 }
